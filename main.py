a6c5f7f207b333bb86b6c552fecc7090cad6b77b
iceicebabyaf <max.nonkin@bk.ru>
2025-09-14 23:03:10 +0700

    main -> one-hot-encoder.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-categories = np.array(['red', 'red', 'blue', 'red'])
-unique = np.array(list(dict.fromkeys(categories.tolist())))
-one_hot = (categories[:, None] == unique[None, :]).astype(int)
-
-print(f'unique categories:{unique}')
-print(one_hot)
